955488a45d9c39a3b22a8f1be06c068529e0fa77
daisuke-shimura <daisuke.s0707@icloud.com>
2025-07-11 19:39:23 +0900

    setting.py修正

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@(zqhw6yj3h_gk_p=)cz04chr!rffha06h(0!tgcx5-35u%@91'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'insecure-local-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
